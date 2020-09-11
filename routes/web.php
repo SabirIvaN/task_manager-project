9bed39728b464c1cbc61a9ffaae6bfd472a5188a
Иван Сабиров <sabirivan@yandex.com>
2020-09-11 21:59:04 +0300

    Fixed connection to the HTTPS protocol

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+if (config('app.env') === 'production') {
+    URL::forceScheme('https');
+}
 
 Auth::routes(['verify' => true]);
 
